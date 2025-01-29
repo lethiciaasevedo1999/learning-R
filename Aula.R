a3e4b920a19d1359f3548a714c5767dfcfda44e3
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2025-01-29 19:45:43 -0300

    Criando matriz para tabela de vendas

--- Aula.R
+++ Aula.R
@@ -31,3 +31,32 @@ rownames(matriz_colab) <- c('Colaboradora Ana', 'Colaborador Carlos', 'Colaborad
 colnames(matriz_colab) <- c('Nome', 'Idade', 'Salário', 'Telefone', 'Trabalho remoto')
 
 matriz_colab
+
+#Criando matriz - Tabela de vendas
+
+# Vetor com valores das vendas
+vendas_jan <- c(20, 18, 25, 16, 23)
+vendas_fev <- c(15, 20, 22, 18, 19)
+vendas_mar <- c(25, 23, 20, 17, 21)
+vendas_abr <- c(18, 15, 19, 20, 24)
+vendas_mai <- c(22, 25, 21, 15, 18)
+vendas_jun <- c(21, 22, 19, 17, 20)
+
+# Nomes das pessoas
+pessoas <- c("Pedro Santos", "Carla Nunes", "Maria Eduarda", "Luiz Felipe", "Julio Costa")
+
+# Nomes dos meses
+meses <- c("Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho")
+
+# Combinar as vendas
+vendas_semestre <- c(vendas_jan, vendas_fev, vendas_mar, vendas_abr, vendas_mai, vendas_jun)
+
+# Vendas de cada pessoa por mês (em milhares)
+matriz_vendas <- matrix(vendas_semestre, nrow = 5, byrow = FALSE)
+
+# Nomear a matriz
+rownames(matriz_vendas) <- pessoas
+colnames(matriz_vendas) <- meses
+
+# Exibir a matriz
+matriz_vendas
